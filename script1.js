c7fc337b63e62a9b2a386150400a5bd79edf2ab7
nakkinn <81859514+nakkinn@users.noreply.github.com>
2022-05-26 17:11:43 +0900

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,15 @@
+let pos=[];
+let facenum=[];
+let col=[];
+let geonum=0;  
+
+function addData(v,f,c=[255,255,255],e=true){
+    pos[geonum]=new Array(v.length);
+    for(let i=0;i<pos[geonum].length;i++)   pos[geonum][i]=[];
+    for(let i=0;i<v.length;i++) for(let j=0;j<3;j++)    pos[geonum][i][j]=v[i][j];
+    facenum[geonum]=new Array(f.length);
+    for(let i=0;i<facenum[geonum].length;i++)   facenum[geonum][i]=[];
+    for(let i=0;i<f.length;i++)   for(let j=0;j<f[i].length;j++) facenum[geonum][i][j]=f[i][j];
+    col[geonum]=c;
+    geonum++;
+}
