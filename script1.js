d2ac4dd722eb2d188f4fc06ee972c67cfa9cd5d4
nakkinn <81859514+nakkinn@users.noreply.github.com>
2022-06-03 14:50:42 +0900

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,17 +1,22 @@
 let pos=[];
 let facenum=[];
+let col=[];
+let edgecol=[];
+let edgewei=[];
 let geonum=0;  
 
-function addData(v,f){
-    /*pos[geonum]=new Array(v.length);
-    for(let i=0;i<pos[geonum].length;i++)   pos[geonum][i]=[];*/
-    if(geonum==0){
-        for(let i=0;i<v.length;i++) pos[i]=[];
-        for(let i=0;i<v.length;i++) for(let j=0;j<3;j++)    pos[i][j]=v[i][j];
-    }
+function addData(v,f,c,ec,ew){
+    pos[geonum]=new Array(v.length);
+    for(let i=0;i<pos[geonum].length;i++)   pos[geonum][i]=[];
+    for(let i=0;i<v.length;i++) for(let j=0;j<3;j++)    pos[geonum][i][j]=v[i][j];
+    
     facenum[geonum]=new Array(f.length);
     for(let i=0;i<facenum[geonum].length;i++)   facenum[geonum][i]=[];
     for(let i=0;i<f.length;i++)   for(let j=0;j<f[i].length;j++) facenum[geonum][i][j]=f[i][j];
+
+    col[geonum]=c;
+    edgecol[geonum]=ec;
+    edgewei[geonum]=ew;
 
     geonum++;
 }
--- script2.js
+++ script2.js
@@ -5,119 +5,91 @@ document.addEventListener('touchmove', disableScroll, { passive: false });
 
 let theta=0,n,size,sw=1;
 let select,check;
+let selectword=[
+    '正十二面体',                   //0
+    '正十二面体面なし',             //1
+    '正十二面体と長方形',           //2
+    '正十二面体と３長方形',         //3
+    '正十二面体・長方形・立方体',   //4
+    '正十二面体と立方体',           //5
+    '正十二面体と５立方体',         //6
+    '正十二面体と正四面体',         //7
+    '正十二面体と５正四面体'        //8
+];
+
+let list=[0];
+let poscopy;
 
 function setup(){
     size=min(windowWidth,windowHeight);
+    if(windowWidth>windowHeight)    size*=0.9;
     createCanvas(size,size,WEBGL);    //キャンバスのサイズ　正方形
     n=createVector(1,0,0);  //回転軸のベクトル
 
-    /*
-    let url=createA('https://google.com','Google'); //リンクの生成　('url','リンクの名前')
-    url.position(0,size);   
-    url.style('font-size',size/25+'px');
-    */
-
     select=createSelect();  //セレクトボックスの生成
     select.style('font-size',size/25+'px');
-    select.option('正十二面体');
-    select.option('正十二面体２');
-    select.option('正十二面体と長方形');
-    select.option('正十二面体と長方形２');
-    select.option('正十二面体と立方体');
+    for(let i=0;i<selectword.length;i++)    select.option(selectword[i]);
+    select.changed(selectevent);
 
-    select.selected('正十二面体');
+    poscopy=[];
+    for(let i=0;i<list.length;i++){
+        poscopy[i]=[];
+        for(let j=0;j<pos[i].length;j++){
+            poscopy[i][j]=pos[list[i]][j].concat();
+        }
+    }
 
-    ortho(-width/2,width/2,-height/2,height/2,0,size*scal*2);
 }
 
 //描写
 function draw(){    
-    background(220);   //背景色
+
+    background(backgroundcolor[0],backgroundcolor[1],backgroundcolor[2]);   //背景色
+    ortho(-width/2,width/2,-height/2,height/2,0,size*scal*2);
 
     scale(size*scal*0.2);   //スケール
 
-    let list=[];
-    if(select.value()=='正十二面体'||select.value()=='正十二面体２')    list=[0];
-    if(select.value()=='正十二面体と長方形')    list=[0,1];
-    if(select.value()=='正十二面体と長方形２')    list=[0,1,2,3];
-    if(select.value()=='正十二面体と立方体')    list=[0,1,2,3,4];
-
-    
     for(let k=0;k<list.length;k++)  for(let i=0;i<facenum[list[k]].length;i++){  
-        strokeWeight(size/100);
-        if(select.value()=='正十二面体'){
-            fill(255,100,100);
-            stroke(0);
-        }
 
-        if(select.value()=='正十二面体２'){
-            noFill();
-            stroke(0);
-        }
-
-        if(select.value()=='正十二面体と長方形'||select.value()=='正十二面体と長方形２'||select.value()=='正十二面体と立方体'){
-            if(k==0){
-                noFill();
-                stroke(0);
-            }
-            if(k==1){
-                fill(255,0,0);
-                noStroke();
-            }
-            if(k==2){
-                fill(0,255,0);
-                noStroke();
-            }
-            if(k==3){
-                fill(0,0,255);
-                noStroke();
-            }
-            if(k==4){
-                noFill();
-                stroke(255,155,0);
-            }
-        }
+        if(col[list[k]]==-1)    noFill();
+        else  fill(col[list[k]]);
+        strokeWeight(edgewei[list[k]]*size/500);
+        if(edgecol[list[k]]==-1)    noStroke();
+        else  stroke(edgecol[list[k]]);
 
         beginShape();
         for(let j=0;j<facenum[list[k]][i].length;j++){
-            vertex(pos[facenum[list[k]][i][j]-1][0],pos[facenum[list[k]][i][j]-1][1],pos[facenum[list[k]][i][j]-1][2]);
+            vertex(pos[list[k]][facenum[list[k]][i][j]-1][0], pos[list[k]][facenum[list[k]][i][j]-1][1], pos[list[k]][facenum[list[k]][i][j]-1][2]);
         }
         endShape(CLOSE);
         
-        if((select.value()=='正十二面体２'||select.value()=='正十二面体と長方形'||select.value()=='正十二面体と長方形２'||select.value()=='正十二面体と立方体')&&k==0){
-            stroke(255);
-            strokeWeight(size*1.5/100);
-            beginShape();
-            for(let j=0;j<facenum[list[k]][i].length;j++){
-                vertex(pos[facenum[list[k]][i][j]-1][0]*0.98, pos[facenum[list[k]][i][j]-1][1]*0.98, pos[facenum[list[k]][i][j]-1][2]*0.98);
-            }
-            endShape(CLOSE);  
-        }
     }
 
 
     //回転
+    
     if(mouseIsPressed==false){  
-        for(let i=0;i<pos.length;i++){
-            let v=createVector(pos[i][0],pos[i][1],pos[i][2]);
-            theta=constrain(theta,0,0.1);
-            v=rot(v,n,theta*deltaTime*0.04);
-            pos[i][0]=v.x;
-            pos[i][1]=v.y;
-            pos[i][2]=v.z;
+        for(let k=0;k<list.length;k++){
+            for(let i=0;i<pos[list[k]].length;i++){
+                let v=createVector(pos[list[k]][i][0], pos[list[k]][i][1], pos[list[k]][i][2]);
+                theta=constrain(theta,0,0.1);
+                v=rot(v,n,theta*deltaTime*0.04);
+                pos[list[k]][i][0]=v.x;
+                pos[list[k]][i][1]=v.y;
+                pos[list[k]][i][2]=v.z;
+            }
         }
-    }else   mousedrag();
+    }else   if(mouseY<size) mousedrag();
 
     //回転の減衰
     if(mouseIsPressed==false){  
         theta*=0.99;
     }
-
     
 }
 
 function mousedrag(){
-
+    
     let angle=atan2(mouseY-pmouseY,mouseX-pmouseX)+PI/2;
     theta=map(dist(mouseX,mouseY,pmouseX,pmouseY),0,width,0,6);
 
@@ -126,18 +98,46 @@ function mousedrag(){
     n.y=sin(angle);
 
     //回転
-    for(let i=0;i<pos.length;i++){   
-        let v=createVector(0,0,0);
-    
-        v=v.set(pos[i][0],pos[i][1],pos[i][2]);
+    for(let k=0;k<list.length;k++){
+        for(let i=0;i<pos[list[k]].length;i++){   
+            let v=createVector(0,0,0);
         
-        v=rot(v,n,theta*deltaTime*0.04);
-        pos[i][0]=v.x;
-        pos[i][1]=v.y;
-        pos[i][2]=v.z;
-    }    
+            v=v.set(pos[list[k]][i][0],pos[list[k]][i][1],pos[list[k]][i][2]);
+            
+            v=rot(v,n,theta*deltaTime*0.04);
+            pos[list[k]][i][0]=v.x;
+            pos[list[k]][i][1]=v.y;
+            pos[list[k]][i][2]=v.z;
+        }  
+    }  
     
 }
+
+
+function selectevent(){
+
+    for(let i=0;i<list.length;i++)  for(let j=0;j<poscopy[i].length;j++)    pos[list[i]][j]=poscopy[i][j].concat();
+
+    if(select.value()==selectword[0])   list=[0];
+    if(select.value()==selectword[1])   list=[1];
+    if(select.value()==selectword[2])   list=[1,2];
+    if(select.value()==selectword[3])   list=[1,2,3,4];
+    if(select.value()==selectword[4])   list=[1,2,3,4,5];
+    if(select.value()==selectword[5])   list=[1,6];
+    if(select.value()==selectword[6])   list=[1,6,7,8,9,10];
+    if(select.value()==selectword[7])   list=[1,11];
+    if(select.value()==selectword[8])   list=[1,11,12,13,14,15];
+
+    poscopy=[];
+    for(let i=0;i<list.length;i++){
+        poscopy[i]=[];
+        for(let j=0;j<pos[i].length;j++){
+            poscopy[i][j]=pos[list[i]][j].concat();
+        }
+    }
+    
+}
+
 
 function rot(v,n,theta){
 
